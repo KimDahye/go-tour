03f393869dac34a7231f966c00789b495bd1b705
Dahye Kim <dahye.sophie.kim@gmail.com>
2018-08-30 16:44:20 +0900

    add comment

--- basics/slices.go
+++ basics/slices.go
@@ -5,6 +5,6 @@ import "fmt"
 func main() {
 	var s []int // slice with element of type int
 	s = []int{2, 3, 4, 5, 6}
-	subset := s[0:3] // {2,3,4}
+	subset := s[0:3] // {2,3,4} - subset은 array가 아니고 slice 임
 	fmt.Println(subset)
 }
